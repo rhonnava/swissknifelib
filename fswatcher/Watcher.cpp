02c70b15513340b3aae74d82113f43ef9bcda960
ravihr <ravihr@gmail.com>
2011-06-19 03:26:37 +0000

    Dev changes

--- fswatcher/Watcher.cpp
+++ fswatcher/Watcher.cpp
@@ -12,20 +12,19 @@ Watcher::Watcher(bool blocking) : m_inotifyFD(0)
 	}
 }
 
-void Watcher::registerHandler(const boost::function<void(void)>& handler )
-{
-	m_handler = handler;
-}
-
 //Register watch type from one of those declared in Watcher.
-bool Watcher::registerWatch(watchtype_t type, const string& watch_)
+bool Watcher::registerWatch(watchtype_t type, const string& watch_, const boost::function<void(void)>& handler_)
 {
+	if(!m_inotifyFD)
+		return false;
+
 	int watchFD = inotify_add_watch(m_inotifyFD, watch_.c_str(), type); 
 
+	//Log an error here when the logging toolkit is done.
 	if(-1 == watchFD)
 		return false;
 
-	m_watchFDs.insert(watchFD);
+	m_watchMap.insert(make_pair(watchFD, handler_));
 	return true;
 }
 
@@ -33,6 +32,8 @@ bool Watcher::run()
 {
 	if(!m_inotifyFD)
 		return false;
+
+	
 
 
 }
