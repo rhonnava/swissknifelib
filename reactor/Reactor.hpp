7c4fb409aba4291b445597bb233bf27ac6eea0cc
ravihr <ravihr@gmail.com>
2011-06-28 06:19:29 +0000

    dev changes

--- reactor/Reactor.hpp
+++ reactor/Reactor.hpp
@@ -1,6 +1,7 @@
 #ifndef _REACTOR_HPP_
 #define _REACTOR_HPP_
 #include <boost/utility.hpp>
+#include <boost/function.hpp>
 
 class Reactor : private boost::noncopyable
 {
@@ -12,13 +13,13 @@ Reactor();
 //Providing a safe bool to check if reactor got constructed properly. 
 //If there was a problem in constructing the Reactor, we do not throw an exception.
 //Instead we set a flag.
-bool();
+operator bool();
 
 bool operator!();
 
 //Registers a handler to watch fd_. It is necessary to have a non bocking fd if we need the watch to be 
 //edge triggered.
-int registerWatch(int type_, int fd_, const boost::function<void(void)>& fn_, bool edgetriggered_=false);
+bool registerWatch(int type_, int fd_, const boost::function<void(void)>& fn_, bool edgetriggered_=false);
 
 int modifyWatch();
 
