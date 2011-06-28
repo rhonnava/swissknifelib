cb0a430ff26cce441cdc334afbd6bce23b31f255
ravihr@gmail.com <ravihr@gmail.com@92af60e4-276b-2829-20db-d54db6d6d116>
2011-06-28 06:19:29 +0000

    dev changes

--- trunk/reactor/Reactor.cpp
+++ trunk/reactor/Reactor.cpp
@@ -1,7 +1,8 @@
 #include "Reactor.hpp"
-#include "defs.hpp"
+#include "internal/defs.hpp"
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 #include <sys/epoll.h>
 
 Reactor::Reactor() : m_ok_(false)
@@ -23,6 +24,7 @@ Reactor::Reactor() : m_ok_(false)
 		case ENOMEM:
 		break;
 		default:
+		break;
 	}
 }
 
@@ -31,13 +33,24 @@ Reactor::operator bool()
 	return m_ok_;
 }
 
-Reactor::operator!()
+bool Reactor::operator!()
 {
 	return !m_ok_;
 }
 
-int Reactor::registerWatch(int type_, int fd_, const boost::function<void(void)>& fn_, bool edgetriggered_)
+bool Reactor::registerWatch(int type_, int fd_, const boost::function<void(void)>& fn_, bool edgetriggered_)
 {
+
+	struct epoll_event event_;
+	event_.events = 0;
+	event_.data.fd = fd_;
+
+	//To avoid an invalid input from crashing epoll, sanitizing type_ by bitwise AND with a collection of
+	// all possible valid inputs
+	type_ & (EPOLLIN | EPOLLOUT | EPOLLRDHUP | EPOLLPRI | EPOLLERR | EPOLLHUP | EPOLLET | EPOLLONESHOT);
+
+	//Set the event type to the bitset
+	event_.events | type_;
 	if(edgetriggered_)
 	{
 		if(!(O_NONBLOCK & fcntl(fd_, F_GETFD)))
@@ -46,7 +59,17 @@ int Reactor::registerWatch(int type_, int fd_, const boost::function<void(void)>
 			//an edge triggered watch an return out an error;
 			return -1;
 		}
-	}	
+
+		//Add the EPOLLET event to the bitset for edge trigger.
+		event_.events | EPOLLET;
+		
+	}
+
+	if(0 == epoll_ctl(m_ePollFD, EPOLL_CTL_ADD, fd_, &event_))
+	{
+		return true;	
+	}
+
 }
 
 Reactor::~Reactor()
